1407e894fef7695044b97ed678cbd83cf658ca3e
leanhhao86 <91060875+haolekatalon@users.noreply.github.com>
2023-10-13 14:10:09 +0700

    Update utils.js

--- dist/utils.js
+++ dist/utils.js
@@ -1,15 +1,14 @@
 "use strict";
-require('dotenv').config();
-
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getRedirectRules = exports.getS3Config = void 0;
+require('dotenv').config();
 function getS3Config({ s3Config }) {
     const { accessKeyId, secretAccessKey, bucket, key, region } = s3Config || {};
     return {
         accessKeyId: accessKeyId || process.env.ACCESSKEYID,
         secretAccessKey: secretAccessKey || process.env.SECRETACCESSKEY,
         bucket: bucket || process.env.BUCKET,
-        key: key ? `${key}.json` : (process.env.ACCESSKEYID ? `${process.env.ACCESSKEYID}.json` :'document_changes.json'),
+        key: key ? `${key}.json` : (process.env.ACCESSKEYID ? `${process.env.ACCESSKEYID}.json` : 'document_changes.json'),
         region: region || (process.env.REGION || 'us-east-2'),
     };
 }
